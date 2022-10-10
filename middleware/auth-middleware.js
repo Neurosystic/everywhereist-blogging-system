c8fb0dcac9ff09c7349cd57984698b7a9c825010
Sophie Chen <xche966@aucklanduni.ac.nz>
2022-10-10 21:34:44 +1300

    Creating skeleton structure files

---
+++ middleware/auth-middleware.js
@@ -0,0 +1,15 @@
+const userDao = require("../modules/users-dao.js");
+
+async function addUserToLocals(req, res, next){
+    const user = await userDao.retrieveUserWithAuthToken(req.cookies.authToken);
+    res.locals.user = user;
+    next();
+}
+
+function verifyAuthenticated(req, res, next){
+    if(res.locals.user){
+        next();
+    } else {
+        res.redirect("./login");
+    }
+}
