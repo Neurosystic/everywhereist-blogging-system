4fa0805b2f0526e7ce6bae5aefd33f8a37ddd1d3
Sophie Chen <xche966@aucklanduni.ac.nz>
2022-10-13 12:16:06 +1300

    created new js for task assignment

---
+++ public/js/articles.js
@@ -0,0 +1,6 @@
+// JS implementation of like button when clicked - toggle class??
+
+// retrieve from local cookie - if(cookie){... like button enable}
+// register like onto database 
+
+//if storing like - to get article id - can use #articleId.textContent
